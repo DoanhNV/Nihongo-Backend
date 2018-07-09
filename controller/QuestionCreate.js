30b7063dd15ea4199e0d05867193cf0b88478497
Ngo Van Doanh <doanh.ngo@dahu.vn>
2018-07-10 00:03:38 +0700

    Development_20180710_DoanhNV: refactor question create

--- controller/QuestionCreate.js
+++ controller/QuestionCreate.js
@@ -6,24 +6,7 @@ export default class QuestionCreate extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          defaultAnswerNumber : 4,
-          defaultTopic : [
-            {name : "Hiragana to Kanji", value : 0},
-            {name : "Kanji to Hiragana", value : 1},
-            {name : "Vocabulary", value : 2},
-            {name : "Synonym", value : 3},
-            {name : "Fill into braces", value : 4},
-            {name : "Replace star", value : 5},
-            {name : "Reading - Understanding", value : 6}
-          ],
-          defaultLevel : [
-            {name : "Beginer", value : 0},
-            {name : "N1", value : 1},
-            {name : "N2", value : 2},
-            {name : "N3", value : 3},
-            {name : "N4", value : 4},
-            {name : "N5", value : 5}
-          ],
+          initData : initData,
           title : "",
           answers : [],
           topic : 0,
@@ -32,10 +15,48 @@ export default class QuestionCreate extends React.Component {
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+    }
 
+    handleChange(e) {
+      var name = e.target.name;
+      var value = e.target.value;
+      this.setState({ [name] : value});
     }
 
     handleSubmit(e) {
+      var formData = this.getFormData();
+      var createQuestionURL = "http://localhost:8282/mvcquestion/create";
+      if(this.isValidData(formData)) {
+        var data  = this.preparePostData(formData);
+        console.log(data)
+        this.postToServer(createQuestionURL, data);
+      }
+      e.preventDefault();
+    }
+
+    preparePostData(formData) {
+      return {
+        title : formData.htmlTitle,
+        topic : this.state.topic,
+        level : this.state.level,
+        answers: formData.answers,
+        headers: {autorizacion: localStorage.token}
+      }
+    }
+
+    postToServer(url, data) {
+      Axios.post(url, data).then (
+          res => {
+          console.log("response: " + res);
+          var alertStr = res.code == 0 ? "Insert success!" : "Insert Fail!";
+          alert(alertStr);
+      }).catch(error => {
+          alert("Server Error!");
+      });
+    }
+
+
+    getFormData() {
       var htmlTitle = ReactDOM.findDOMNode(document.getElementById("editor")).innerHTML;
       var checkBoxes = document.querySelectorAll("#isCorrects");
       var contents = document.querySelectorAll("#answers");
@@ -43,7 +64,6 @@ export default class QuestionCreate extends React.Component {
       
       for(var i = 0; i < checkBoxes.length; i++) {
         if(contents[i].value === "") {
-          alert(i);
           answers = [];
           break;
         }
@@ -51,31 +71,20 @@ export default class QuestionCreate extends React.Component {
         answers.push(answer);
       }
 
-      if(htmlTitle === "" || answers.length == 0) {
-        alert("please input correct data!" + this.state.title + " - " + answers.length);
-      } else {
-        var data  = {
-          title : htmlTitle,
-          topic : this.state.topic,
-          level : this.state.level,
-          answers: answers,
-          headers: {autorizacion: localStorage.token}
-        }
-        console.log(data)
-        Axios.post("http://localhost:8282/mvcquestion/create", data).then (
-          res => {
-          alert("Insert success!");
-        }).catch(error => {
-          alert("Insert Fail!");
-        });
+      return {
+        htmlTitle : htmlTitle,
+        checkBoxes : checkBoxes,
+        contents : contents,
+        answers : answers
       }
-      e.preventDefault();
     }
 
-    handleChange(e) {
-      var name = e.target.name;
-      var value = e.target.value;
-      this.setState({ [name] : value});
+    isValidData(formData) {
+      if(formData.htmlTitle === "" || formData.answers.length == 0) {
+        alert("please input correct data!");
+        return false;
+      }
+      return true;
     }
 
     render() {
@@ -104,7 +113,7 @@ export default class QuestionCreate extends React.Component {
                       <label class="control-label col-lg-1" for="inputSuccess"><b>Answer</b></label>
                       <div class="col-lg-10">
                         {
-                          Array(this.state.defaultAnswerNumber).fill(2).map((i) => {
+                          Array(this.state.initData.defaultAnswerNumber).fill(2).map((i) => {
                             return (
                                   <div class="checkbox">
                                     <label>
@@ -121,9 +130,10 @@ export default class QuestionCreate extends React.Component {
                       <label class="control-label col-lg-1" for="inputSuccess"><b>Topic</b></label>
                       <div class="col-lg-10">
                         <select name="topic" class="form-control m-bot15" onChange={this.handleChange}>
-                            {this.state.defaultTopic.map((topic) => {
+                            {this.state.initData.defaultTopic.map((topic) => {
                               return <option value={topic.value}>{topic.name}</option>
                             })}
+                            {this.initData}
                         </select>
                       </div>
                     </div>
@@ -133,7 +143,7 @@ export default class QuestionCreate extends React.Component {
                       <label class="control-label col-lg-1" for="inputSuccess"><b>Level</b></label>
                       <div class="col-lg-10">
                         <select name="level" class="form-control m-bot15" onChange={this.handleChange}>
-                            {this.state.defaultLevel.map((level) => {
+                            {this.state.initData.defaultLevel.map((level) => {
                               return <option value={level.value}>{level.name}</option>
                             })}
                         </select>
@@ -153,5 +163,26 @@ export default class QuestionCreate extends React.Component {
         </div>
         );
     }
+}
+
+const initData = {
+  defaultAnswerNumber : 4,
+  defaultTopic : [
+    {name : "Hiragana to Kanji", value : 0},
+    {name : "Kanji to Hiragana", value : 1},
+    {name : "Vocabulary", value : 2},
+    {name : "Synonym", value : 3},
+    {name : "Fill into braces", value : 4},
+    {name : "Replace star", value : 5},
+    {name : "Reading - Understanding", value : 6}
+  ],
+  defaultLevel : [
+    {name : "Beginer", value : 0},
+    {name : "N1", value : 1},
+    {name : "N2", value : 2},
+    {name : "N3", value : 3},
+    {name : "N4", value : 4},
+    {name : "N5", value : 5}
+  ]
 }
 
