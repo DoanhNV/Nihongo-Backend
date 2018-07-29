211a502726f889e5cce366865c62da5e52598d6b
Ngo Van Doanh <doanh.ngo@dahu.vn>
2018-07-29 17:59:31 +0700

    development_20180729_DoanhNV: create template

--- controller/QuestionList.js
+++ controller/QuestionList.js
@@ -3,68 +3,76 @@ import React from 'react';
 export default class QuestionList extends React.Component {
     render() {
         return (
+          <div>
             <div class="row">
-            <div class="col-lg-12">
-              <section class="panel">
-                {/* Header title */}
-                <header class="panel-heading">
-                  Create
-                </header>
-                <div class="panel-body">
-                  <div class="form">
-                    <form class="form-validate form-horizontal " id="register_form" method="get" action="" novalidate="novalidate">
-                      <div class="form-group ">
-                        <label for="fullname" class="control-label col-lg-2">Full name <span class="required">*</span></label>
-                        <div class="col-lg-10">
-                          <input class=" form-control" id="fullname" name="fullname" type="text"/>
-                        </div>
-                      </div>
-                      <div class="form-group ">
-                        <label for="address" class="control-label col-lg-2">Address <span class="required">*</span></label>
-                        <div class="col-lg-10">
-                          <input class=" form-control" id="address" name="address" type="text"/>
-                        </div>
-                      </div>
-                      <div class="form-group ">
-                        <label for="username" class="control-label col-lg-2">Username <span class="required">*</span></label>
-                        <div class="col-lg-10">
-                          <input class="form-control " id="username" name="username" type="text"/>
-                        </div>
-                      </div>
-                      <div class="form-group ">
-                        <label for="password" class="control-label col-lg-2">Password <span class="required">*</span></label>
-                        <div class="col-lg-10">
-                          <input class="form-control " id="password" name="password" type="password"/>
-                        </div>
-                      </div>
-                      <div class="form-group ">
-                        <label for="confirm_password" class="control-label col-lg-2">Confirm Password <span class="required">*</span></label>
-                        <div class="col-lg-10">
-                          <input class="form-control " id="confirm_password" name="confirm_password" type="password"/>
-                        </div>
-                      </div>
-                      <div class="form-group ">
-                        <label for="email" class="control-label col-lg-2">Email <span class="required">*</span></label>
-                        <div class="col-lg-10">
-                          <input class="form-control " id="email" name="email" type="email"/>
-                        </div>
-                      </div>
-                      <div class="form-group ">
-                        <label for="agree" class="control-label col-lg-2 col-sm-3">Agree to Our Policy <span class="required">*</span></label>
-                        <div class="col-lg-10 col-sm-9">
-                          <input type="checkbox" width="20px" class="checkbox form-control" id="agree" name="agree"/>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="col-lg-offset-2 col-lg-10">
-                          <button class="btn btn-primary" type="submit">Save</button>
-                          <button class="btn btn-default" type="button">Cancel</button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
+              <div class="col-lg-12">
+                  <h3 class="page-header"> Question list</h3>
+              </div>
+            </div>
+              <div class="row">
+              <div class="col-lg-12">
+                <div class="profile-widget profile-widget-info">
+                    <div class="panel-body">
+                      Query condition
+                    </div>
                 </div>
-              </section>
+              </div>
+              <div class="col-lg-12">
+                <section class="panel">
+                  {/* <div class="row query-area question-list-query">
+                      sdfsfd
+                  </div> */}
+                  <div class="panel-body">
+                    {/* item */}
+                    <div class="tab-content">
+                      <div id="recent-activity">
+                        <div id="profile" class="tab-pane">
+                            <section class="panel"> 
+                                <div class="bio-graph-heading question-title">
+                                    Hello I’m Jenifer Smith, a leading expert in interactive and creative design specializing in the mobile medium. My graduation from Massey University with a Bachelor of Design majoring in visual communication.
+                                </div>
+                                <div class="panel-body bio-graph-info">
+                                    <p class="question-sub"><span>Sub </span>: Jenifer </p>
+                                    {/* Answer */}
+                                    <h1 class="group-title">Answers</h1>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <p><span>First Name </span>: Jenifer </p>
+                                        </div>
+                                    </div>
+                                    {/* Infomation */}
+                                    <h1 class="group-title">Infomations</h1>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                          <p>
+                                            <span>Level </span>: Jenifer <br/>
+                                            <span>Topic </span>: Jenifer 
+                                          </p>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <p><img id="previewImage" height="70" class="show"/></p>
+                                        </div>
+                                    </div>
+                                </div>
+                              </section>
+                          </div>
+                      </div>
+                    </div>
+                    <div class="row paging-container">
+                      <div class="pagination">
+                        <a href="#">&laquo;</a>
+                        <a href="#">1</a>
+                        <a class="active" href="#">2</a>
+                        <a href="#">3</a>
+                        <a href="#">4</a>
+                        <a href="#">5</a>
+                        <a href="#">6</a>
+                        <a href="#">&raquo;</a>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
             </div>
           </div>
         ); 
